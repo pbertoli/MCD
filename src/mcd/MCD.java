6006797e55e9ee291b8f6d4eefdffb2a8257f124
Amministratore <Amministratore@PC-060.homenet.telecomitalia.it>
2018-10-16 13:11:20 +0200

    Schema iniziale

---
+++ src/mcd/MCD.java
@@ -0,0 +1,15 @@
+
+package mcd;
+
+public class MCD {
+
+    public static void main(String[] args) {
+        
+    }
+    public static int mcd (   )  {
+        
+    }
+}
+class ExceptionParameters extends Exception    {
+    
+}
