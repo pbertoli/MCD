853cc115bc6cb7e30ae554d9621c95a3f686f209
Amministratore <Amministratore@PC-060.homenet.telecomitalia.it>
2018-10-16 13:30:03 +0200

    MCD risolto

--- src/mcd/MCD.java
+++ src/mcd/MCD.java
@@ -1,15 +1,25 @@
-
 package mcd;
 
 public class MCD {
 
     public static void main(String[] args) {
-        
+        System.out.println(mcd(0, 12));
     }
-    public static int mcd (   )  {
-        
+
+    public static int mcd(int a, int b) {
+        if (a < b) {
+            int c = a;
+            a = b;
+            b = c;
+        }
+        if (b == 0) {
+            return a;
+        } else {
+            return mcd(b, a % b);
+        }
     }
 }
-class ExceptionParameters extends Exception    {
-    
+
+class ExceptionParameters extends Exception {
+
 }
