d0f2dbe5ca2cd3f15c23579b0b61a5b1b8a90459
Amministratore <Amministratore@PC-060.homenet.telecomitalia.it>
2018-10-17 10:08:03 +0200

    MCD risolto con asserzioni

--- src/mcd/MCD.java
+++ src/mcd/MCD.java
@@ -3,23 +3,61 @@ package mcd;
 public class MCD {
 
     public static void main(String[] args) {
-        System.out.println(mcd(0, 12));
+        try {
+            System.out.println(mcd(-46, 12));
+        } catch (ExceptionMcd e) {
+            e.urlaErrore();
+        }
     }
 
-    public static int mcd(int a, int b) {
+    public static int mcd(int a, int b)
+            throws ExceptionMcd {
+        // verifica delle precondizioni
+        if (a < 0) {
+            throw new ExceptionMcd(1);
+        }
+        if (b < 0) {
+            throw new ExceptionMcd(2);
+        }
         if (a < b) {
             int c = a;
             a = b;
             b = c;
         }
         if (b == 0) {
+            // postcondizioni
             return a;
         } else {
-            return mcd(b, a % b);
+            // postcondizioni
+            int mcd = mcd(b, a % b);
+            assert mcd >=0: "Errore di calcolo interno: mcd negativo.";
+            assert a % mcd == 0 && b % mcd == 0:
+                    "Errore: l'mcd non è nemmeno un divisore.";
+            return mcd;
         }
     }
-}
 
-class ExceptionParameters extends Exception {
+    private static class ExceptionMcd extends Exception {
 
+        public ExceptionMcd(int nError) {
+            String s = "";
+            switch (nError) {
+                case 1:
+                    s = "Primo parametro negativo";
+                    break;
+                case 2:
+                    s = "Secondo parametro negativo";
+                    break;
+                case 3:
+                    s = "valore mcd negativo";
+                    break;
+                default:
+                    s = "Eccezione non gestita";
+            }
+            System.out.println("Metodo MCD: " + s);
+        }
+        public void urlaErrore() {
+            System.out.println("PORCA MISERIA");
+        }
+    }
 }
